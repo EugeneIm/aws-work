7428e081c4651fbaa465883a20e458c07c25bb0b
Eugene Im <eugeneim@eugenes-air.ad.bcit.ca>
2022-02-03 14:06:19 -0800

    aws repo created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import './index.css';
 
 function App() {
+
+  const [file, setFile] = useState()
+  const [images, setImages] = useState([]);
+
+
+  useEffect(() => {
+    async function getImages() {
+      const result = await axios.get("https://ctzxthsbvk.execute-api.us-west-1.amazonaws.com/dev/images");
+      const images = result.data.images;
+      setImages(images);
+    }
+    getImages();
+  }, []);
+
+
+
+//POOL ID: us-west-1_Z0GSiBSv6
+//APP CLIENT ID: 7kdc7spa63ukqae62091qfi9ve
+
+
+
+const submit = async event => {
+  event.preventDefault()
+  // get secure url from our server
+  const result = await axios.get("https://ctzxthsbvk.execute-api.us-west-1.amazonaws.com/dev/signedurl")
+  const url = result.data.url
+  console.log(url)
+
+
+
+  // post the image direclty to the s3 bucket
+  await axios.put(url, file, { headers: { "Content-Type": file.type } })
+
+  // get the image url and add it to the images array
+  const imageUrl = url.split("?")[0]
+  setImages([...images, imageUrl])
+}
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        <input onChange={e => setFile(e.target.files[0])}  type="file" accept="image/*"></input>
+        <button type="submit">Submit</button>
+      </form>
+      {images.map((image) => (
+        <img alt="" key={image} src={image}></img>
+      ))}
     </div>
   );
 }
